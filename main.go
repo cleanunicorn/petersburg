5e81f65253b89d0f299f6d6276a20fab9ff05ecb
Daniel Luca <lucadanielcostin@gmail.com>
2018-03-25 19:08:51 +0200

    Add lives

--- main.go
+++ main.go
@@ -4,17 +4,20 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+
+	humanize "github.com/dustin/go-humanize"
 )
 
 var (
 	sum   int64
 	pulls int64
 	avg   float64
+	lives = 3
 )
 
 func playGame() int {
 	var p int
-	for p = 1; (rand.Intn(2) != 0) && (p <= 3); p++ {
+	for p = 1; (rand.Intn(2) != 0) && (p <= lives); p++ {
 	}
 	return p
 }
@@ -29,35 +32,36 @@ func pow2(exp int) int64 {
 
 func main() {
 	histogram := make(map[int]int)
-	for i := 0; i < 1; i++ {
-		go func() {
-			var max int64
-			max = 0
-			for {
-				pulls++
-				tries := playGame()
-				var p int64
-				if tries == 4 {
-					p = 0
-				} else {
-					p = pow2(tries - 1)
-				}
-				histogram[tries]++
-				sum += p
-				avg = float64(sum) / float64(pulls)
-				if max < p {
-					// fmt.Println("[!] Max", tries, p)
-					max = p
-				}
-				if pulls%10000000 == 0 {
-					fmt.Println(pulls, avg, max)
-					fmt.Println(histogram)
-					fmt.Println("tries =", tries, "won =", p)
-					max = 0
-				}
+	go func() {
+		var max int64
+		max = 0
+		for {
+			pulls++
+			tries := playGame()
+			var p int64
+			if tries == lives+1 {
+				p = 0
+			} else {
+				p = pow2(tries - 1)
 			}
-		}()
-	}
+			histogram[tries]++
+			sum += p
+			avg = float64(sum) / float64(pulls)
+			if max < p {
+				// fmt.Println("[!] Max", tries, p)
+				max = p
+			}
+			if pulls%10000000 == 0 {
+				fmt.Println("----------------")
+				fmt.Printf("pulls = %s\tavg = %g\tmax = %s\n", humanize.Comma(pulls), avg, humanize.Comma(max))
+				for i := 1; i < len(histogram)+1; i++ {
+					fmt.Printf("%d\t%s\n", i, humanize.Comma(int64(histogram[i])))
+				}
+				// fmt.Println("tries =", humanize.Comma(int64(tries)), "won =", humanize.Comma(p))
+				max = 0
+			}
+		}
+	}()
 
 	go func() {
 		var previous int64
